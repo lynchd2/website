412311fff94f866f2dcf1fb8c3a6cef8fab765f0
Dylan Lynch <lynchd2@tcnj.edu>
2016-04-11 22:39:50 -0400

    Basic Scripting #1-8 finished using ruby

---
+++ FreeCodeCampRubyConversions/basic_algorithm_scripting.rb
@@ -0,0 +1,146 @@
+#These methods are the answers to the the Basic Algorithm Scripting section
+#but in Ruby instead of Javascript
+
+#1 Reverse a string
+def reverse(str)
+  str.reverse
+end
+
+#puts reverse("This")
+
+#2 Factorial a given number
+
+def fn(num)
+  array = []
+  product = 1
+  until num == 0
+    array << num
+	num -= 1
+  end
+  array.each do |num|
+	product = num * product
+  end
+return product
+end
+
+# or using recursion
+
+def fn(num)
+  return 1 if num == 0
+  num * fn(num - 1)
+end
+
+# or another way
+
+def fn(num)
+  total = 1
+  while num > 0
+    total *= num
+    num -=1
+  end
+  total
+end
+
+#p fn(5)
+
+#3 Palindrome
+
+def rev(str)
+  new_string = str.gsub(/(\s|,|\.)/,"")
+  newer_string = str.gsub(/(\s|,|\.)/,"").split("").reverse.join("")
+  p newer_string === new_string
+end
+
+#rev("never odd or even")
+
+#4 Longest word in a string
+
+def longest(str)
+  count = 0
+  str.split(" ").each {|word| count = word.length if word.length > count}
+  p count
+end
+
+#longest("this is a sentence with sentence as the longestish word jk it's actually longestish")
+
+#5 Capitalize first word in a string
+
+def cap(str)
+  str.split(" ").each {|word| word.capitalize!}.join(" ")
+end
+
+#p cap("let's SEE if this works")
+
+#6 Biggest number in array of arrays of numbers
+def biggest_number(num)
+  numbers = []
+  num.each do |array_num|
+    biggest = 0
+    array_num.each do |num|
+      if num > biggest
+        biggest = num
+      end
+    end
+    numbers << biggest
+  end
+  p numbers
+end
+
+biggest_number([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+
+#7 Does target match end of string
+
+def sub_str_tar(str, target)
+  #p str[((str.length - 1) - (target.length - 1))..(str.length - 1)] == target
+  #or a shorter version
+  p str.slice(str.length - target.length, target.length)
+end
+
+sub_str_tar("He has to give me a new name", "me")
+
+#7 Repeat a given string the given number of times
+
+def times_string(str, num)
+  p str * num
+end
+
+times_string("abc", 4)
+
+#8 
+#Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+#Return the truncated string with a ... ending.
+
+#Note that inserting the three dots to the end 
+#will add to the string length.
+
+#However, if the given maximum string length num 
+#is less than or equal to 3, then the addition 
+#of the three dots does not add to the string length in determining the truncated string.
+
+def string_sentence(str, num)
+  if str.length > num
+    if num <= 3
+      return str[0..num - 1] << "..."
+    end
+    return str[0..num - 4] << "..."
+  end
+end
+
+p string_sentence("A-", 1)
+
+#9 Write a function that splits an array (first argument)
+#into groups the length of size (second argument) and 
+#returns them as a two-dimensional array.
+
+def splitting(arr, size)
+  total = 0
+  final_array = []
+  until total >= arr.length
+    part_of_arr = arr[total..total + 1]
+    final_array << part_of_arr
+    total += size
+  end
+  return final_array
+end
+
+p splitting(["a", "b", "c", "d"], 2)
