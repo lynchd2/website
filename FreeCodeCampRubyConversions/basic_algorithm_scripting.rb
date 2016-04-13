4ee7dce3d2db9c4b6282ee68a1f5cceeb5d72ceb
Dylan Lynch <lynchd2@tcnj.edu>
2016-04-13 13:04:42 -0400

    Make user profile page responsive

--- FreeCodeCampRubyConversions/basic_algorithm_scripting.rb
+++ FreeCodeCampRubyConversions/basic_algorithm_scripting.rb
@@ -86,7 +86,7 @@ def biggest_number(num)
   p numbers
 end
 
-biggest_number([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
+# biggest_number([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])
 
 #7 Does target match end of string
 
@@ -96,7 +96,7 @@ def sub_str_tar(str, target)
   p str.slice(str.length - target.length, target.length)
 end
 
-sub_str_tar("He has to give me a new name", "me")
+# sub_str_tar("He has to give me a new name", "me")
 
 #7 Repeat a given string the given number of times
 
@@ -104,7 +104,7 @@ def times_string(str, num)
   p str * num
 end
 
-times_string("abc", 4)
+# times_string("abc", 4)
 
 #8 
 #Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
@@ -126,7 +126,7 @@ def string_sentence(str, num)
   end
 end
 
-p string_sentence("A-", 1)
+# p string_sentence("A-", 1)
 
 #9 Write a function that splits an array (first argument)
 #into groups the length of size (second argument) and 
@@ -143,4 +143,74 @@ def splitting(arr, size)
   return final_array
 end
 
-p splitting(["a", "b", "c", "d"], 2)
+#p splitting(["a", "b", "c", "d"], 2)
+
+#10 Take give number of elements from array
+
+def arr_with_given(array, num)
+  array.take(array.length - num)
+end
+
+#arr_with_given([1,2,3,4,5], 1)
+
+#11 Return true if the string in the first element of the
+#array contains all of the letters of the string in the second element of the array.
+
+def mutation(arr)
+  first = arr[0]
+  second = arr[1]
+  count = 0
+  first.downcase.split("").each do |letter1|
+    second.downcase.split("").each do |letter2|
+      if letter1 == letter2
+        count += 1
+      end
+    end
+  end
+  return count
+end
+
+# p mutation(["Mary", "Army"])
+
+#12 Filter an array of all false values, leaving only true values
+
+def only_true(array)
+  array.reject{|element| element == false || element == nil}
+end
+
+# p only_true(["five", 9, nil, false, 2 > 3])
+
+#13 Where do I belong- Return the lowest index at which a value (second argument)
+#should be inserted into an array (first argument) once it has been sorted.
+
+def get_index(arr, num)
+  arr.sort.each_with_index do |arr_num, index| 
+    if arr_num > num
+      return index
+    else
+      return arr.length
+    end
+  end
+end
+p get_index([2, 5, 10], 15)
+
+#14 caesar cipher using only capitals
+
+def c_c(str)
+  final_string = []
+  letters = ("A".."Z").to_a
+  str.split("").each do |letter|
+    if letters.include?(letter)
+      if letters.index(letter) + 13 < 26
+        final_string << letters[letters.index(letter) + 13]
+      else
+      final_string << letters[letters.index(letter) - 13]
+    end
+    else
+      final_string << letter
+    end
+  end
+  return final_string.join("")
+end
+
+# c_c("GUR DHVPX OEBJA QBT WHZCRQ BIRE GUR YNML SBK,")
