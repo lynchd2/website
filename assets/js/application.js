b89c6b91b44ac7deb173d5bedd8c294d04067964
Dylan Lynch <lynchd2@tcnj.edu>
2016-05-14 18:24:35 -0400

    Finish about page with other skills. Add mail icon to homepage with link to email. Fix bracket hover effect on homepage. Start project page with one started and three empty spaces for other projects.

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  /*
   $(".brackets-name").hide();
   $(".brackets-occupation").hide();
   $(".dylan-lynch")
@@ -15,17 +16,30 @@ $(document).ready(function(){
     .mouseleave(function() {
       $(".brackets-occupation").fadeOut("fast")
   });
+*/
 
-  $(".github-black").mouseenter(function() {
-    $(this).addClass("github-white")
-    $(this).removeClass("github-black")
+$(".dylan-lynch").hover(function(){
+  $('.brackets-name').addClass("name-occupation-hovered")
+}, function() {
+  $('.brackets-name').removeClass("name-occupation-hovered")
+});
+
+$(".occupation-container").hover(function(){
+  $(".brackets-occupation").addClass("name-occupation-hovered")
+}, function() {
+  $(".brackets-occupation").removeClass("name-occupation-hovered")
+});
+
+  $(".github-black").hover(function() {
+    $(this).addClass("github-white", "slow")
+    $(this).removeClass("github-black", "slow")
   }).mouseleave(function() {
-    $(this).addClass("github-black")
-    $(this).removeClass("github-white")
+    $(this).addClass("github-black", "slow")
+    $(this).removeClass("github-white", "slow")
   });
 
 $('.arrowBottom').click(function () {
-    $('html, body').animate({scrollTop:$(document).height() - 655}, 'slow');
+    $('html, body').animate({scrollTop:$(document).height() - 630}, 'slow');
     return false;
 });
 
