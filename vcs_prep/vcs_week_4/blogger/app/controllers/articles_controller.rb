d61129397498819057302b1f5fa655c5851474c4
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-25 17:15:53 -0400

    Finish chapter 11 of jumpstart tutorial

--- vcs_prep/vcs_week_4/blogger/app/controllers/articles_controller.rb
+++ vcs_prep/vcs_week_4/blogger/app/controllers/articles_controller.rb
@@ -1,12 +1,20 @@
 class ArticlesController < ApplicationController
+
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
+    flash.notice = "Article '#{@article.title}' created!"
+    redirect_to(@article)
   end
 
   def show
@@ -14,11 +22,22 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash.notice = "Article '#{@article.title}' destroyed!"
+    redirect_to articles_path
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' updated!"
+    redirect_to articles_path
   end
+
+
 end
